45dd9c4cf0f0cc6b001eef3122f179ff75056f10
jeancharlescano <jeancharles.cano@outlook.fr>
2025-09-04 17:51:59 +0200

    Reload des commandes sur les serveurs au démarrage du bot

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,12 @@
 import { Events } from "discord.js";
 import getTimestamp from "../utils/date.js"
+import { deployAllGuildCommands } from "../deploy-commands.js";
 
 export default {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
+    async execute (client) {
         console.log(`[${getTimestamp()}] 🟩 Ready! Logged in as ${client.user.tag}`);
+        await deployAllGuildCommands(client);
     }
 }
